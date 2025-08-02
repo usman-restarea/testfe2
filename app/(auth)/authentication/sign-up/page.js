9c57c4688455117cc9f0e1f68f7445cb730f93da
usman-restarea <usmanhidayatulloh@gmail.com>
2025-08-03 05:02:23 +0700

    token

--- app/(auth)/authentication/sign-up/page.js
+++ app/(auth)/authentication/sign-up/page.js
@@ -17,7 +17,7 @@ const SignUp = () => {
   });
 
   
-  const REFRESH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTY3Mjc2NjAyOCwiZXhwIjoxNjc0NDk0MDI4fQ.kCak9sLJr74frSRVQp0_27BY4iBCgQSmoT3vQVWKzJg';
+  const REFRESH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTY3Mjc2NjAyOCwiZXhwIjoxNjcyODAyMDI4fQ.P1_rB3hJ5afwiG4TWXLq6jOAcVJkvQZ2Z-ZZOnQ1dZw";
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
